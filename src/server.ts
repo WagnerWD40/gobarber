7e134295195e620c7cd9661e7943f42cc3a3d25e
WagnerWD40 <wagnerwd40@gmail.com>
2020-07-17 15:40:18 -0300

    added appointment routes, model and repository

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import routes from './routes';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(routes);
 
 app.get('/', (req, res) => {
     return res.json({ message: 'Hello world!' });
